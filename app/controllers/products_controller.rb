d3535e9af24b126cff5f0c5d3794e8a368bcbc37
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2014-09-03 14:40:28 -0400

    Displaying price in dollars

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,7 +18,7 @@ class ProductsController < ApplicationController
   def create
   	@product = Product.new(product_params)
 
-  		if product.save
+  		if @product.save
   			redirect_to products_url
   		else
   			render :new
@@ -46,5 +46,5 @@ class ProductsController < ApplicationController
   def product_params
   	params.require(:product). permit(:name, :description, :price_in_cents)
   end
-  
+
 end
